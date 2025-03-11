8513bd98b7840401866a8cb21d52ddb5249b0f29
NatWilsonr <nat.wr@hotmail.com>
2025-03-11 14:04:54 -0600

    Agregado: impresion del arbol AVL despues de insertar y borrar.

--- src/arbolAVL/pruebaImpresionAVL.java
+++ src/arbolAVL/pruebaImpresionAVL.java
@@ -16,7 +16,7 @@ public class pruebaImpresionAVL {
         // Crear un arbol AVL de enteros
         ArbolAVL<Integer> arbol = new ArbolAVL<>();
 
-        // Insertar elementos
+        // Insertar elementos y verificar el balanceo
         System.out.println("Insertando elementos...");
         int[] elementos = {30, 20, 40, 10, 25, 35, 50, 5, 15, 27};
         for (int elem : elementos) {
@@ -27,6 +27,33 @@ public class pruebaImpresionAVL {
         // Imprimir el arbol por niveles con los factores de equilibrio
         System.out.println("\nImprimiendo arbol AVL por niveles:");
         arbol.imprimirPorNiveles();
+
+        // Buscar elementos
+        System.out.println("\nBuscando elementos...");
+        int[] busquedas = {10, 25, 50, 100};
+        for (int elem : busquedas) {
+            System.out.println("Elemento " + elem + " encontrado: " + arbol.buscar(elem));
+        }
+
+        // Eliminar elementos y verificar el balanceo
+        System.out.println("\nEliminando elementos...");
+        int[] eliminaciones = {10, 20, 30};
+        for (int elem : eliminaciones) {
+            arbol.eliminar(elem);
+            System.out.println("Eliminado: " + elem);
+        }
+
+        // Imprimir el arbol nuevamente despues de eliminaciones
+        System.out.println("\nImprimiendo arbol AVL despues de eliminaciones:");
+        arbol.imprimirPorNiveles();
+
+        // Buscar elementos despues de eliminaciones
+        System.out.println("\nBuscando despues de eliminaciones...");
+        for (int elem : busquedas) {
+            System.out.println("Elemento " + elem + " encontrado: " + arbol.buscar(elem));
+        }
+        
+        System.out.println("\nPruebas completadas.");
     }
     
 }
